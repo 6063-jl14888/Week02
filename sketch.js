313f9fe68b217f7bf2b72f96ab343a4986688cf2
jinyuan2333 <jl14888@nyu.edu>
2023-09-13 15:25:04 -0400

    class work: added two ellipse and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
+    createCanvas(600, 600);
+    background(153, 0, 153)
 }
 
 function draw() {
+    stroke(0);
+    fill("orange");
+    ellipse(100, 100, 100, 100);
+    fill(0, 225, 0);
+    ellipse(600, 100, 300, 300);
+    stroke(255, 0, 0);
+    strokeWeight(4);
+    rect(300, 300, 200, 200);
 }
