4387366c0f7660866dcf2f6a6b90a4b59c293660
jinyuan2333 <jl14888@nyu.edu>
2023-09-13 16:56:43 -0400

    classwork

--- sketch.js
+++ sketch.js
@@ -1,15 +1,71 @@
+let eyeWidth = 50;
+let eyeWidth1 = 100;
+let faceWidth = 400;
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(windowWidth, windowHeight);
     background(153, 0, 153)
 }
 
 function draw() {
+    fill(153)
+    noStroke();
+    ellipse(mouseX,mouseY,50,50);
+
+    fill(0);
+    rect(0,0,50,50);
+    rect(600 - 50, 0, 50, 50);
+    rect(600 - 50, 600-50, 50, 50);
+    rect(0, 600-50, 50, 50);
+
+    fill("blue")
     stroke(0);
+    ellipse(300, 300, faceWidth, faceWidth);
+
+    fill(255);
+    ellipse(120, 350, eyeWidth1, eyeWidth1);
+    ellipse(280, 350, eyeWidth1, eyeWidth1);
+
     fill("orange");
-    ellipse(100, 100, 100, 100);
-    fill(0, 225, 0);
-    ellipse(600, 100, 300, 300);
-    stroke(255, 0, 0);
-    strokeWeight(4);
-    rect(300, 300, 200, 200);
+    ellipse(100, 360, eyeWidth, eyeWidth);
+    ellipse(260, 360, eyeWidth, eyeWidth);
+
+    noFill();
+    
+    strokeWeight(6)
+    arc(150, 300, 120, 100, PI + QUARTER_PI, TWO_PI);
+    arc(320, 300, 120, 100, PI + QUARTER_PI, TWO_PI);
+
+    strokeWeight(4)
+    fill("red");
+    triangle(180, 400, 220, 400, 225, 450)
+
+  
+
+translate(400, 0)
+
+    fill("blue")
+    stroke(0);
+    ellipse(300, 300, faceWidth, faceWidth);
+
+    fill(255);
+    ellipse(120, 350, eyeWidth1, eyeWidth1);
+    ellipse(280, 350, eyeWidth1, eyeWidth1);
+
+    fill("orange");
+    ellipse(100, 360, eyeWidth, eyeWidth);
+    ellipse(260, 360, eyeWidth, eyeWidth);
+
+    noFill();
+    
+    strokeWeight(6)
+    arc(150, 300, 120, 100, PI + QUARTER_PI, TWO_PI);
+    arc(320, 300, 120, 100, PI + QUARTER_PI, TWO_PI);
+
+    fill("red");
+    triangle(180, 400, 220, 400, 225, 450)
+
+
+
+ 
 }
